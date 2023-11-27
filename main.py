5b7353546b3f18d27421e81d18fe214149c2ce4a
Folonok <144366501+Folonok@users.noreply.github.com>
2023-11-27 20:51:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow,QInputDialog
 from ui import Ui_MainWindow
 
 import json
@@ -18,6 +18,8 @@ class NoteWidget(QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.add_note)
         self.ui.pushButton_5.clicked.connect(self.save_note)
         self.ui.pushButton_6.clicked.connect(self.del_note)
+        self.ui.pushButton_4.clicked.connect(self.add_tag)
+        self.ui.pushButton_3.clicked.connect(self.del_tag)
     def read_notes(self):
         try:
             with open("notes.txt", "r", encoding="utf-8") as file:
@@ -47,17 +49,38 @@ class NoteWidget(QMainWindow):
     def save_note(self):
         title = self.ui.lineEdit.text()
         text = self.ui.textEdit.toPlainText()
-
-        self.notes[title] = {"текст": text, "теги": []}
+        if title not in self.notes:
+            self.notes[title] = {"текст": text, "теги": []}
+        else:
+            self.notes[title]["текст"] = text
         self.save_file()
-        self.ui.lineEdit.clear()
-        self.ui.lineEdit.addItems(self.notes)
+        self.ui.listWidget.clear()
+        self.ui.listWidget.addItems(self.notes)
 
     def del_note(self):
          title = self.ui.lineEdit.text()
-         if title in self.notes[title]:
+         if title in self.notes:
             del self.notes[title]
             self.save_file()
+            self.ui.listWidget.clear()
+            self.ui.listWidget.addItems(self.notes)
+    def add_tag(self):
+        title = self.ui.lineEdit.text()
+        tag_title, ok = QInputDialog.getText(self, "Введіть тег", "Назва тега")
+        if ok and tag_title != "" and title != "":
+            self.notes[title]["теги"].append(tag_title)
+            self.ui.listWidget_2.clear()
+            self.ui.listWidget_2.addItems(self.notes[title]["теги"])
+    def del_tag(self):
+        title = self.ui.lineEdit.text()
+        try:
+            tag_title = self.ui.listWidget_2.selectedItems()[0].text()
+        except:
+            tag_title = None
+        if tag_title and title != "":
+            self.notes[title]["теги"].remove(tag_title)
+            self.ui.listWidget_2.clear()
+            self.ui.listWidget_2.addItems(self.notes[title]["теги"])
 app = QApplication([])
 ex = NoteWidget()
 ex.show()
